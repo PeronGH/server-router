c062bc931e654257c0c4d746a0b0ba337402c1be
peron <peron_ap@icloud.com>
2023-02-28 21:57:32 +0800

    first commit

---
+++ mod.ts
@@ -0,0 +1,2 @@
+export { ServerRouter } from "./src/router.ts";
+export type { ServerHandler } from "./src/router.ts";
---
+++ src/router.ts
@@ -0,0 +1,28 @@
+export class ServerRouter {
+  private routerMap: Map<string, ServerHandler> = new Map();
+
+  route(path: string, handler: ServerHandler) {
+    if (!path.startsWith("/")) throw new Error("Path must start with /");
+    this.routerMap.set(path, handler);
+    return this;
+  }
+
+  handle(req: Request): Promise<Response> {
+    const { url } = req;
+    const { pathname } = new URL(url);
+
+    const sortedMap = [...this.routerMap].sort(([a], [b]) =>
+      b.length - a.length
+    );
+
+    for (const [path, handler] of sortedMap) {
+      if (pathname.startsWith(path)) {
+        return handler(req);
+      }
+    }
+
+    return Promise.resolve(new Response("Not Found", { status: 404 }));
+  }
+}
+
+export type ServerHandler = (req: Request) => Promise<Response>;
